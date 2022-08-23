79595128a9ba3cbc130711a19bec7c2ccf5baa68
Ritualisti <pele.lotjonen@gmail.com>
2022-08-23 15:34:35 +0300

    2. Muuttujat ja vuorovaikutteiset ohjelmat 1-6

---
+++ Teht_2.5.py
@@ -0,0 +1,15 @@
+#leiviskä = 8512 g
+#naula = 425.6 g
+#luoti = 13.3 g
+
+leiviskät = float(input("Anna leiviskät: "))
+naulat = float(input("Anna naulat: "))
+luodit = float(input("Anna luodit: "))
+
+massa = (leiviskät * 8512) + (naulat * 425.6) + (luodit * 13.3)
+
+print(massa)
+
+
+
+
--- Teht_2.6.py
+++ Teht_2.6.py
@@ -1,4 +1,12 @@
 import random
 
 x = random.randint(0, 9)
-print(x)
+y = random.randint(0,9)
+z = random.randint(0, 9)
+print(x,y,z)
+
+q = random.randint(1,6)
+w = random.randint(1,6)
+e = random.randint(1,6)
+r = random.randint(1,6)
+print(q,w,e,r)
